f11bfbb46cd4706aa201cf01fab082e5dc58147e
Sif Høg <cimmik@yahoo.dk>
2023-12-10 23:48:45 +0100

    p5play imported, canvas adjusted
    Sprite doesn't display correctly. Maybe import isn't right. Look into other projects with all files.

--- sketch-control-test.js
+++ sketch-control-test.js
@@ -1,6 +1,25 @@
+let ball, floor;
+
+
+
 function setup() {
     const DISPLAY = document.getElementById("diaplay-control-test");
     let canvas = createCanvas(DISPLAY.offsetWidth, DISPLAY.offsetHeight);
     canvas.parent("diaplay-control-test");
-    
+    background(backgroundColour);
+    addEventListener("resize", (event) => {
+        canvas.resize(DISPLAY.offsetWidth, DISPLAY.offsetHeight);
+        background(backgroundColour);
+    });
+
+
+    world.gravity.y = 10;
+	
+	ball = new Sprite(40, 30, 50);
+    fill(255, 10, 10);
+    circle(100, 30, 50);
+}
+
+function draw() {
+    clear();
 }
