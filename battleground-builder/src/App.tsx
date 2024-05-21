d807483ff9a55d61bd9ebedee25330de6e8c0eb8
Sif Høg <cimmik@yahoo.dk>
2024-05-21 17:16:02 +0200

    Create instance map

--- battleground-builder/src/App.tsx
+++ battleground-builder/src/App.tsx
@@ -2,8 +2,29 @@ import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import "./App.css";
 import Titlebar from "./components/Titlebar";
 import AssetCards from "./components/AssetCards";
+import { useState } from "react";
+import { TankConfig, BarrierConfig } from "./assets/tank-assets";
+
+interface TankInstance extends TankConfig {
+  pos: { x: number; y: number };
+  rotation: number;
+}
+interface BarrierInstance extends BarrierConfig {
+  pos: { x: number; y: number };
+  rotation: number;
+}
+type AssetInstance = TankInstance | BarrierInstance;
 
 function App() {
+  const [assetInstances, setAssetInstances] = useState<
+    Map<string, AssetInstance>
+  >(new Map());
+  function addInstance(key: string, asset: AssetInstance) {
+    const NEW_INSTANCES = new Map([...assetInstances.entries()]);
+    NEW_INSTANCES.set(key, asset);
+    setAssetInstances(NEW_INSTANCES);
+  }
+
   return (
     <>
       <Titlebar tabs={["New", "Save", "Save as", "Load"]} />
@@ -48,3 +69,4 @@ function App() {
 }
 
 export default App;
+export type { TankInstance, BarrierInstance, AssetInstance };
